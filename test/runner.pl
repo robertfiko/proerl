f3ef8b1a631446e112fec645905b9417209a0a07
Robert Fiko <fiko.robert@gmail.com>
2023-01-29 00:42:53 +0100

    Extending test cases

--- test/runner.pl
+++ test/runner.pl
@@ -13,8 +13,23 @@ pass(Term) :- Term -> true;(write('FAILED'), halt).
 neg(Term) :- not(Term) -> true; (write('FAILED'), halt).
 
 run_tests(_) :-
+    % ATTRIBUTE
     pass(erl:attribute([-,module,'(',simple,')','.'], '<MOD>'(simple))),
     neg(erl:attribute([-,module,'(',simple,')','.'], '<MOD>'(simple_random))),
-    %pass(erl:attribute([-,export,'(','[',foo,/,1,']',')','.'], <EXP>'(ExportList))),
-    %neg(erl:attribute([-,cica,'(',simple,')','.'])),
+    pass(erl:attribute([-,export,'(','[',foo,/,1,']',')','.'], '<EXPORT>'([foo,/,1]))),
+    neg(erl:attribute([-,cica,'(',simple,')','.'], _)),
+
+    % FUNCTION
+    pass(erl:take_until_funbody([[korte, ','], [5, '+', 6, '.'], [szilva]], [[korte,','],[5,+,6,'.']])),
+
+
+    % ARITHMETICS
+    pass(erl:group_expression([5, '+', '(', 4, '-', 3, ')'], '<EXPR>'(5,+,'<EXPR>'(4,-,3)))),
+    pass(erl:group_expression([5, '+', '(', '(', 4, '-', 1, ')', '/', 2, ')'], '<EXPR>'(5,+,'<EXPR>'('<EXPR>'(4,-,1),/,2)))),
+    pass(erl:group_expression(['(', 5, '+', '(', 4, '-', 1, ')', ')', '/', 2], '<EXPR>'('<EXPR>'(5,+,'<EXPR>'(4,-,1)),/,2))),
+
+
+
+
+
     write('ALL TESTS PASSED').

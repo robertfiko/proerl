dfcdbdfb6410d1ddf043ccd4807de95a81fd60e8
Robert Fiko <fiko.robert@gmail.com>
2023-01-29 00:13:33 +0100

    exmaples files on arithmetic and module def

--- examples/arithmetics.erl
+++ examples/arithmetics.erl
@@ -2,12 +2,10 @@
 -export([foo/1, main/0]).   
 
 foo(_Alma) ->
-    5+4, % expression
+    5+4,
     683*2.
 
 main() ->
     foo(whatever).
 
 
-% [[-,module,'(',arithmetics,')','.'],[-,export,'(','[',foo,/,1,',',main,/,0,']',')','.'],[],[foo,'(','_Alma',')',->],[5,+,4,',','%',expression],[683,*,2,'.'],[],[main,'(',')',->],[foo,'(',whatever,')','.'],[],[]]
-% 
